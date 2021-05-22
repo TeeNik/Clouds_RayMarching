95eb3172db090a343436beab41d9bb29a20e77e0
TeeNik <teenik3315@gmail.com>
2021-05-22 04:31:30 +0300

    animation reader

---
+++ Assets/Scripts/AnimationReader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AnimationReader : MonoBehaviour
+{
+
+    public Animation Animation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Animation != null)
+        {
+        }
+    }
+}
