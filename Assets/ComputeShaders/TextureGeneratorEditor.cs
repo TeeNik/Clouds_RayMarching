d7e1ebb502c51a46deeaddd3110739696c98b900
TeeNik <teenik3315@gmail.com>
2022-03-12 00:19:19 +0300

    generator editor

---
+++ Assets/ComputeShaders/TextureGeneratorEditor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TextureGenerator))]
+public class TextureGeneratorEditor : Editor
+{
+    public TextureGenerator Generator;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Update"))
+        {
+            Generator.Generate();
+        }
+    }
+    void OnEnable()
+    {
+        Generator = (TextureGenerator)target;
+    }
+}
