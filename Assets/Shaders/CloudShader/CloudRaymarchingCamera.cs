57114fa9bf0875b67187a79169d1b668238303ca
TeeNik <teenik3315@gmail.com>
2022-03-22 03:08:01 +0300

    raymarching camera script

---
+++ Assets/Shaders/CloudShader/CloudRaymarchingCamera.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class CloudRaymarchingCamera : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private Slider sunSpeedSlider = null;
+    [SerializeField] private Slider coverageSlider = null;
+    [SerializeField] private Slider densitySlider = null;
+    [SerializeField] private Slider absortionSlider = null;
+    [SerializeField] private Slider jitterSlider = null;
+    [SerializeField] private Toggle taaToggle = null;
+
+    [Header("Components")]
+    [SerializeField] private Transform sun = null;
+    [SerializeField] private PostProcessLayer ppLayer = null;
+    [SerializeField] private Transform sphere = null;
+    [SerializeField] private Transform cube = null;
+    [SerializeField] private TextureGenerator textureGenerator = null;
+
+    public Vector3 Index;
+    public Shader Shader;
+    private Material raymarchMat;
+
+    private readonly int posId = Shader.PropertyToID("_SpherePos");
+    private readonly int radiusId = Shader.PropertyToID("_SphereRadius");
+    private readonly int cubeMinBound = Shader.PropertyToID("_CubeMinBound");
+    private readonly int cubeMaxBound = Shader.PropertyToID("_CubeMaxBound");
+    private readonly int coverageId = Shader.PropertyToID("_Coverage");
+    private readonly int densityId = Shader.PropertyToID("_Density");
+    private readonly int absortionId = Shader.PropertyToID("_Absortion");
+    private readonly int jitterId = Shader.PropertyToID("_JitterEnabled");
+    private readonly int frameCountId = Shader.PropertyToID("_FrameCount");
+
+    private void Start()
+    {
+        raymarchMat = new Material(Shader);
+
+        if (textureGenerator)
+        {
+            Texture3D noiseTexture = textureGenerator.Generate();
+            raymarchMat.SetTexture("_Volume", noiseTexture);
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 eulers = new Vector3(0.0f, sunSpeedSlider.value * Time.deltaTime, 0.0f);
+        sun.Rotate(eulers, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (textureGenerator)
+            {
+                Texture3D noiseTexture = textureGenerator.Generate();
+                raymarchMat.SetTexture("_Volume", noiseTexture);
+            }
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        raymarchMat.SetVector(posId, sphere.position);
+        raymarchMat.SetFloat(radiusId, sphere.localScale.x * 0.5f);
+        raymarchMat.SetVector(cubeMinBound, cube.position - cube.localScale * 0.5f);
+        raymarchMat.SetVector(cubeMaxBound, cube.position + cube.localScale * 0.5f);
+        raymarchMat.SetFloat(coverageId, coverageSlider.value);
+        raymarchMat.SetFloat(densityId, densitySlider.value);
+        raymarchMat.SetFloat(absortionId, absortionSlider.value);
+        raymarchMat.SetInt(jitterId, (int)jitterSlider.value);
+        raymarchMat.SetFloat(frameCountId, Time.frameCount);
+        ppLayer.antialiasingMode = taaToggle.isOn ? PostProcessLayer.Antialiasing.TemporalAntialiasing : PostProcessLayer.Antialiasing.None;
+
+        raymarchMat.SetVector("_Index", Index);
+
+        Graphics.Blit(source, destination, raymarchMat);
+    }
+}
