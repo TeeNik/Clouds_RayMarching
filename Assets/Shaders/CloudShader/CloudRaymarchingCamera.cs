38cd4536f576b81a72954555f1d9fff632d1bff0
TeeNik <teenik3315@gmail.com>
2022-03-24 05:16:35 +0300

    cloud camera script

--- Assets/Shaders/CloudShader/CloudRaymarchingCamera.cs
+++ Assets/Shaders/CloudShader/CloudRaymarchingCamera.cs
@@ -25,6 +25,20 @@ public class CloudRaymarchingCamera : MonoBehaviour
     public Shader Shader;
     private Material raymarchMat;
 
+    private Camera _camera;
+    public Camera Camera
+    {
+        get
+        {
+            if (!_camera)
+            {
+                _camera = GetComponent<Camera>();
+            }
+            return _camera;
+        }
+    }
+    public float MaxDistance;
+
     private readonly int posId = Shader.PropertyToID("_SpherePos");
     private readonly int radiusId = Shader.PropertyToID("_SphereRadius");
     private readonly int cubeMinBound = Shader.PropertyToID("_CubeMinBound");
@@ -63,6 +77,12 @@ public class CloudRaymarchingCamera : MonoBehaviour
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        Graphics.Blit(source, destination, raymarchMat);
+
+        raymarchMat.SetMatrix("_CamFrustum", CamFrustum(Camera));
+        raymarchMat.SetMatrix("_CamToWorld", Camera.cameraToWorldMatrix);
+        raymarchMat.SetFloat("_MaxDistance", MaxDistance);
+
         raymarchMat.SetVector(posId, sphere.position);
         raymarchMat.SetFloat(radiusId, sphere.localScale.x * 0.5f);
         raymarchMat.SetVector(cubeMinBound, cube.position - cube.localScale * 0.5f);
@@ -76,6 +96,57 @@ public class CloudRaymarchingCamera : MonoBehaviour
 
         raymarchMat.SetVector("_Index", Index);
 
-        Graphics.Blit(source, destination, raymarchMat);
+        Matrix4x4 rotMatrix = Matrix4x4.TRS(
+            Vector3.one,
+            Quaternion.Euler(new Vector3(0, (Time.time * 100) % 360, (Time.time * 100) % 360)),
+            Vector3.one
+            );
+
+        raymarchMat.SetMatrix("_RotationMat", rotMatrix.inverse);
+
+
+        RenderTexture.active = destination;
+        raymarchMat.SetTexture("_Background", source);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        raymarchMat.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //bottom left
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+        //bottom right
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+        //top right
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+        //top left
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    Matrix4x4 CamFrustum(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 topLeft = -Vector3.forward - goRight + goUp;
+        Vector3 topRight = -Vector3.forward + goRight + goUp;
+        Vector3 bottomLeft = -Vector3.forward - goRight - goUp;
+        Vector3 bottomRight = -Vector3.forward + goRight - goUp;
+
+        frustum.SetRow(0, topLeft);
+        frustum.SetRow(1, topRight);
+        frustum.SetRow(2, bottomRight);
+        frustum.SetRow(3, bottomLeft);
+
+        return frustum;
     }
 }
