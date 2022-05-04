e57a667a2ac9f2bef1822bf1fdc7b6ed7c7bff9e
TeeNik <teenik3315@gmail.com>
2022-05-04 05:44:50 +0300

    Revert "update texture generator diff"
    This reverts commit d03d05526f5d6731445ac076bee8e7414e75155e.

--- Assets/ComputeShaders/TextureGenerator.cs
+++ Assets/ComputeShaders/TextureGenerator.cs
@@ -1,9 +1,11 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class TextureGenerator : MonoBehaviour
 {
+    public int Resolution;
     public ComputeShader ComputeShader;
     public string KernelName;
     public string AssetName;
@@ -12,55 +14,41 @@ public class TextureGenerator : MonoBehaviour
     private RenderTexture Texture;
     private const int threadGroupSize = 32;
 
-    public NoiseSettings Settings;
-
-    public System.Action<Texture3D> OnSettingsChanged;
-    
-    private NoiseSettings prevSettings;
-
-    protected void IsSettingsChanged()
-    {
-        bool isChanged = !Settings.Equals(prevSettings);
-        if(isChanged)
-        {
-            prevSettings = (NoiseSettings)Settings.Clone();
-            print("Changed");
-            OnSettingsChanged?.Invoke(Generate());
-        }
-    }
-
-    protected void Update()
-    {
-        IsSettingsChanged();
-    }
+    public float Coverage;
+    public int Octaves;
+    public float Frequency;
+    public float Lacunarity;
+    public float Amplitude;
+    public float Persistence;
+    public Vector3 Index;
 
     public Texture3D Generate()
     {
         CreateRenderTexture();
         int kernel = ComputeShader.FindKernel(KernelName);
         ComputeShader.SetTexture(kernel, "Result", Texture);
-        ComputeShader.SetInt("Resolution", Settings.Resolution);
+        ComputeShader.SetInt("Resolution", Resolution);
 
-        ComputeShader.SetFloat("Coverage", 1.0f - Settings.Coverage);
-        ComputeShader.SetInt("Octaves", Settings.Octaves);
-        ComputeShader.SetFloat("Frequency", Settings.Frequency);
-        ComputeShader.SetFloat("Lacunarity", Settings.Lacunarity);
-        ComputeShader.SetFloat("Amplitude", Settings.Amplitude);
-        ComputeShader.SetFloat("Persistence", Settings.Persistence);
-        ComputeShader.SetVector("CellIndex", Settings.Index);
+        ComputeShader.SetFloat("Coverage", 1.0f - Coverage);
+        ComputeShader.SetInt("Octaves", Octaves);
+        ComputeShader.SetFloat("Frequency", Frequency);
+        ComputeShader.SetFloat("Lacunarity", Lacunarity);
+        ComputeShader.SetFloat("Amplitude", Amplitude);
+        ComputeShader.SetFloat("Persistence", Persistence);
+        ComputeShader.SetVector("CellIndex", Index);
 
         ComputeShader.GetKernelThreadGroupSizes(kernel, out uint xGroupSize, out uint yGroupSize, out uint zGroupSize);
-        ComputeShader.Dispatch(kernel, Settings.Resolution / (int)xGroupSize, Settings.Resolution / (int)yGroupSize, Settings.Resolution / (int)zGroupSize);
+        ComputeShader.Dispatch(kernel, Resolution / (int)xGroupSize, Resolution / (int)yGroupSize, Resolution / (int)zGroupSize);
 
         return SaveAsset();
     }
 
-    protected void CreateRenderTexture()
+    public void CreateRenderTexture()
     {
         var format = UnityEngine.Experimental.Rendering.GraphicsFormat.R16G16B16A16_UNorm;
-        Texture = new RenderTexture(Settings.Resolution, Settings.Resolution, 0);
+        Texture = new RenderTexture(Resolution, Resolution, 0);
         Texture.graphicsFormat = format;
-        Texture.volumeDepth = Settings.Resolution;
+        Texture.volumeDepth = Resolution;
         Texture.enableRandomWrite = true;
         Texture.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
         Texture.name = name;
@@ -71,44 +59,47 @@ public class TextureGenerator : MonoBehaviour
 
     protected Texture2D ConvertFromRenderTexture(RenderTexture rt)
     {
-        Texture2D output = new Texture2D(Settings.Resolution, Settings.Resolution);
+        Texture2D output = new Texture2D(Resolution, Resolution);
         RenderTexture.active = rt;
-        output.ReadPixels(new Rect(0, 0, Settings.Resolution, Settings.Resolution), 0, 0);
+        output.ReadPixels(new Rect(0, 0, Resolution, Resolution), 0, 0);
         output.Apply();
         return output;
     }
 
-    protected Texture3D SaveAsset()
+    public Texture3D SaveAsset()
     {
         string sceneName = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene().name;
         AssetName = sceneName + "_" + AssetName;
-        Texture2D[] slices = new Texture2D[Settings.Resolution];
+        Texture2D[] slices = new Texture2D[Resolution];
 
         int kernel = Slicer.FindKernel("CSMain");
-        Slicer.SetInt("resolution", Settings.Resolution);
+        Slicer.SetInt("resolution", Resolution);
         Slicer.SetTexture(kernel, "Noise", Texture);
 
-        for (int layer = 0; layer < Settings.Resolution; layer++)
+        for (int layer = 0; layer < Resolution; layer++)
         {
-            var slice = new RenderTexture(Settings.Resolution, Settings.Resolution, 0);
+            var slice = new RenderTexture(Resolution, Resolution, 0);
             slice.dimension = UnityEngine.Rendering.TextureDimension.Tex2D;
             slice.enableRandomWrite = true;
             slice.Create();
 
             Slicer.SetTexture(kernel, "Result", slice);
             Slicer.SetInt("Layer", layer);
-            int numThreadGroups = Mathf.CeilToInt(Settings.Resolution / (float)threadGroupSize);
+            int numThreadGroups = Mathf.CeilToInt(Resolution / (float)threadGroupSize);
             Slicer.Dispatch(kernel, numThreadGroups, numThreadGroups, 1);
 
             slices[layer] = ConvertFromRenderTexture(slice);
 
         }
 
-        Texture3D output = Tex3DFromTex2DArray(slices, Settings.Resolution);
+        Texture3D output = Tex3DFromTex2DArray(slices, Resolution);
+        //AssetDatabase.CreateAsset(output, "Assets/ComputeShaders/" + name + ".asset");
+        //AssetDatabase.SaveAssets();
+        //AssetDatabase.Refresh();
         return output;
     }
 
-    protected Texture3D Tex3DFromTex2DArray(Texture2D[] slices, int resolution)
+    Texture3D Tex3DFromTex2DArray(Texture2D[] slices, int resolution)
     {
         Texture3D tex3D = new Texture3D(resolution, resolution, resolution, TextureFormat.ARGB32, false);
         tex3D.filterMode = FilterMode.Trilinear;
