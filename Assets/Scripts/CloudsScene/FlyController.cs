4207f82bd843eded88b9bb61d55e5853b7f538ff
TeeNik <teenik3315@gmail.com>
2022-06-03 00:28:08 +0300

    vertical fly

--- Assets/Scripts/CloudsScene/FlyController.cs
+++ Assets/Scripts/CloudsScene/FlyController.cs
@@ -9,10 +9,25 @@ public class FlyController : MonoBehaviour
     public float MaxOffset = 30;
     public float Speed = 10;
 
+    public float MaxRadiusOffset = 1;
+    public float VerticalSpeed = 20;
+
+    private float DefaultRotZ = 10;
+
     private float AngleOffset;
+    private Vector3 InitialPos;
+    private float RadiusOffset;
+
+    private void Start()
+    {
+        InitialPos = transform.position;
+    }
 
     void Update()
     {
+        float targetZRot = DefaultRotZ;
+        float prevRadiusOffset = RadiusOffset;
+
         if(Input.GetKey(KeyCode.A))
         {
             AngleOffset -= Time.deltaTime * Speed;
@@ -21,16 +36,39 @@ public class FlyController : MonoBehaviour
         {
             AngleOffset += Time.deltaTime * Speed;
         }
+        if( Input.GetKey(KeyCode.W))
+        {
+            RadiusOffset += Time.deltaTime * VerticalSpeed;
+            targetZRot = 0;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            RadiusOffset -= Time.deltaTime * VerticalSpeed;
+            targetZRot = 20;
+        }
 
         AngleOffset = Mathf.Clamp(AngleOffset, -MaxOffset, MaxOffset);
         float angle = DefaultAngle + AngleOffset;
 
-        float x = Radius * Mathf.Cos(Mathf.Deg2Rad * angle);
-        float y = Radius * Mathf.Sin(Mathf.Deg2Rad * angle);
-        transform.position = new Vector3(x, y, transform.position.z);
+        RadiusOffset = Mathf.Clamp(RadiusOffset, -MaxRadiusOffset, MaxRadiusOffset);
+        float radDiff = Mathf.Abs(Mathf.Abs(RadiusOffset) - MaxRadiusOffset);
+        print(radDiff);
+        if (radDiff < 0.001)
+        {
+            targetZRot = DefaultRotZ;
+        }
+
+        float radius = Radius + RadiusOffset;
+
+        float x = radius * Mathf.Cos(Mathf.Deg2Rad * angle);
+        float y = radius * Mathf.Sin(Mathf.Deg2Rad * angle);
+        transform.position = InitialPos + new Vector3(x, y, 0);
 
         Vector3 rot = transform.eulerAngles;
-        rot.z = AngleOffset;
-        transform.eulerAngles = rot;
+        rot.x = AngleOffset;
+        rot.z = targetZRot;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,
+            Quaternion.Euler(rot), Time.deltaTime * Speed);
+        //transform.eulerAngles = rot;
     }
 }
