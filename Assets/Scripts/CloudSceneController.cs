af0188436dd8136312c5080e6c39628be799538a
TeeNik <teenik3315@gmail.com>
2022-06-09 05:03:15 +0300

    balloon spawner

--- Assets/Scripts/CloudSceneController.cs
+++ Assets/Scripts/CloudSceneController.cs
@@ -47,16 +47,37 @@ public class CloudSceneController : MonoBehaviour
     public AnimationCurve EmissionIntensity;
     public float DayDuration = 5.0f;
 
+    [Header("Balloon Settings")]
+    public int BalloonNum = 5;
+    public Balloon BalloonPrefab;
+    public float BalloonSpawnRate = 5.0f;
+    public Transform Player;
+
     [Header("References")]
     public CloudRaymarchingCamera CloudRaymarching;
     public List<BalloonInfo> BalloonInfos;
 
     private float CurrentDayTime = 0;
 
+    private List<Balloon> BalloonPool = new List<Balloon>();
+    private float BalloonSpawnTime = 0.0f;
+
+    private void Start()
+    {
+        for (int i = 0; i < BalloonNum; i++)
+        {
+            var balloon = Instantiate(BalloonPrefab, transform);
+            BalloonPool.Add(balloon);
+        }
+    }
 
     void Update()
     {
-        CurrentDayTime += Time.deltaTime;
+        //if (CurrentDayTime < DayDuration / 2.0f)
+        {
+            CurrentDayTime += Time.deltaTime;
+        }
+
         float dayPercent = Mathf.Clamp01(CurrentDayTime / DayDuration);
 
         var skyTop = SkyTopColor.Evaluate(dayPercent);
@@ -67,14 +88,31 @@ public class CloudSceneController : MonoBehaviour
         RenderSettings.skybox.SetColor("_BottomColor", skyBottom);
         RenderSettings.skybox.SetFloat("_StarsIntensity", starsIntensity);
 
+        CloudRaymarching.CloudsColor = CloudsColor.Evaluate(dayPercent);
+
         foreach(var balloonInfo in BalloonInfos)
         {
-            balloonInfo.Move(dayPercent);
+            //balloonInfo.Move(dayPercent);
 
             //var pos = balloonInfo.Object.position;
             //var yValue = balloonInfo.HeightCurve.Evaluate(dayPercent);
             //pos.y = Mathf.Lerp(balloonInfo.MinY, balloonInfo.MaxY, yValue);
             //balloonInfo.Object.position = pos;
+        }
+
+        BalloonSpawnTime -= Time.deltaTime;
+        if(BalloonSpawnTime <= 0.0f)
+        {
+            BalloonSpawnTime = BalloonSpawnRate;
+            foreach ( var balloon in BalloonPool)
+            {
+                if(!balloon.IsActive)
+                {
+                    print("Balloon spawned");
+                    balloon.StartFlight(Player);
+                    break;
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/CloudsScene/Balloon.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public float MinSpeed = 0.1f; 
+    public float MaxSpeed = 0.3f;
+
+    public Vector3 MinBound;
+    public Vector3 MaxBound;
+
+    //public float MinDist;
+    //public float MaxDist;
+    //public float MinSideOffet;
+    //public float MaxSideOffet;
+    //public float MinHeight;
+    //public float MaxHeight;
+
+    public Texture2D[] Textures;
+
+    public bool IsActive { get; private set; }
+
+    private float Speed;
+
+    public void StartFlight(Transform player)
+    {
+        IsActive = true;
+        Speed = Random.Range(MinSpeed, MaxSpeed);
+
+        float dist = Random.Range(MinBound.z, MaxBound.z);
+        Vector3 pos = player.transform.position + player.transform.forward * dist;
+        pos.y = MinBound.y;
+        Vector3 side = Random.Range(0, 2) > 0 ? player.right : -player.right;
+        pos += side * Random.Range(MinBound.x, MaxBound.y);
+        transform.position = pos;
+    }
+
+    void Update()
+    {
+        if(IsActive)
+        {
+            transform.position += Vector3.up * Speed * Time.deltaTime;
+            if(transform.position.y > MaxBound.y)
+            {
+                IsActive=false;
+            }
+        }
+    }
+}
