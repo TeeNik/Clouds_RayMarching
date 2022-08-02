4f11688830cd0aac183fba8b9b6a4408eb5e98aa
TeeNik <teenik3315@gmail.com>
2022-08-03 01:36:53 +0300

    smooth movement

--- Assets/Scripts/CloudsScene/FlyController.cs
+++ Assets/Scripts/CloudsScene/FlyController.cs
@@ -8,9 +8,11 @@ public class FlyController : MonoBehaviour
     public float RollSpeed = 45;
     public float MaxVerticalOffset = 0.05f;
     public float VerticalSpeed = 20;
+    
     public float DefaultPitch = -10.0f;
-    public float MaxPitch = 0.0f;
-    public float MinPitch = -20.0f;
+    public float PitchOffset = 10.0f;
+    public float PitchSpeed = 5.0f;
+    
     public float TargetRoll = 30.0f;
 
     [Header("Camera")]
@@ -23,12 +25,9 @@ public class FlyController : MonoBehaviour
 
     private Vector3 InitialPos;
     private float RadiusOffset;
-    private float CurrentCameraRotSpeed;
 
     private float RollValue = 0.0f;
-
-    public float MinDelayAfterPress = 1.5f;
-    private float DelayAfterPress = 0;
+    private float PitchValue = 0.0f;
 
     private void Start()
     {
@@ -37,69 +36,50 @@ public class FlyController : MonoBehaviour
 
     void Update()
     {
-        float targetRoll = 0;
-        float targetPitch = DefaultPitch;
-
         Vector3 dist = Camera.transform.forward * Time.deltaTime * CameraMovementSpeed;
         Camera.transform.position += dist;
         Cube.transform.position += dist;
 
         if (Input.GetKey(KeyCode.A))
         {
-            targetRoll = TargetRoll;
-
-            CurrentCameraRotSpeed -= Time.deltaTime * CameraRotationSpeed;
             RollValue += Time.deltaTime * RollSpeed;
-            DelayAfterPress = MinDelayAfterPress;
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            targetRoll = -TargetRoll;
-
-            CurrentCameraRotSpeed += Time.deltaTime * CameraRotationSpeed;
             RollValue -= Time.deltaTime * RollSpeed;
-            DelayAfterPress = MinDelayAfterPress;
         }
         else
         {
-            CurrentCameraRotSpeed += CurrentCameraRotSpeed > 0 ? -(Time.deltaTime * CameraRotationSpeedDamping) : Time.deltaTime * CameraRotationSpeedDamping;
             RollValue = RollValue > 0 ? RollValue - Time.deltaTime : RollValue + Time.deltaTime;
-            //RollValue -= Time.deltaTime;
         }
 
         RollValue = Mathf.Clamp(RollValue, -1.0f, 1.0f);
-        print(RollValue);
-        CurrentCameraRotSpeed = Mathf.Clamp(CurrentCameraRotSpeed, -MaxCameraRotationSpeed, MaxCameraRotationSpeed);
-        Camera.transform.Rotate(Vector3.up, CurrentCameraRotSpeed);
+        float currentCameraRotSpeed = -RollValue * MaxCameraRotationSpeed;
+        Camera.transform.Rotate(Vector3.up, currentCameraRotSpeed);
 
         if ( Input.GetKey(KeyCode.W))
         {
-            RadiusOffset += Time.deltaTime * VerticalSpeed;
-            targetPitch = MaxPitch;
+            PitchValue += Time.deltaTime * PitchSpeed ;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            RadiusOffset -= Time.deltaTime * VerticalSpeed;
-            targetPitch = MinPitch;
+            PitchValue -= Time.deltaTime * PitchSpeed;
         }
-
-
-        RadiusOffset = Mathf.Clamp(RadiusOffset, -MaxVerticalOffset, MaxVerticalOffset);
-        float radDiff = Mathf.Abs(Mathf.Abs(RadiusOffset) - MaxVerticalOffset);
-        if (radDiff < 0.001)
+        else
         {
-            targetPitch = DefaultPitch;
+            PitchValue = PitchValue > 0 ? PitchValue - Time.deltaTime : PitchValue + Time.deltaTime;
         }
+
+        RadiusOffset += Time.deltaTime * VerticalSpeed * PitchValue;
+        PitchValue = Mathf.Clamp(PitchValue, -1.0f, 1.0f);
+        RadiusOffset = Mathf.Clamp(RadiusOffset, -MaxVerticalOffset, MaxVerticalOffset);
 
         transform.localPosition = InitialPos - Vector3.up * RadiusOffset;
 
         Vector3 rot = transform.eulerAngles;
-        rot.x = targetPitch;
-        rot.z = targetRoll;
+        rot.x = DefaultPitch + PitchValue * PitchOffset;
         rot.z = TargetRoll * RollValue;
 
         transform.rotation = Quaternion.Euler(rot);
-
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rot), Time.deltaTime * RollSpeed);
     }
 }
